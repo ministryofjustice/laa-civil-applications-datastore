26f9634103e5e5be6c39ab473d1ff06654c07b82
RoseSAK <rose.azadkan@digital.justice.gov.uk>
2024-07-25 12:25:55 +0100

    Disable seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-REF_NUMBERS = %w[AB-123456 CD-987654 EF-345678]
-
-REF_NUMBERS.each do |ref|
-  app = CivilApplication.new(id: SecureRandom.uuid, reference_number: ref, employment_income: rand(50000))
-  app.save!
-  sleep(5)
-end
+# REF_NUMBERS = %w[AB-123456 CD-987654 EF-345678]
+#
+# REF_NUMBERS.each do |ref|
+#   app = CivilApplication.new(id: SecureRandom.uuid, reference_number: ref, employment_income_amount: rand(50000))
+#   app.save!
+#   sleep(5)
+# end
